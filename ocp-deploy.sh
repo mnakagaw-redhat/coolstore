a768a2c371702b3025001176d985bb2a84f5c042
mnakagaw <mnakagaw@redhat.com>
2025-09-04 15:31:50 +0900

    修正

--- ocp-deploy.sh
+++ ocp-deploy.sh
@@ -12,11 +12,11 @@ JAR=$(ls -1 target/*.jar)
 # Create keycloak.json from template
 envsubst < keycloak.template.json > src/main/resources/META-INF/resources/keycloak.json
 
-# oc new-build registry.access.redhat.com/ubi8/openjdk-21 --strategy source --binary --name ${APP_NAME} --dry-run -o yaml | oc apply -f -
+oc new-build registry.access.redhat.com/ubi8/openjdk-21 --strategy source --binary --name ${APP_NAME} --dry-run -o yaml | oc apply -f -
 oc start-build ${APP_NAME} --from-file=$JAR --follow
-# oc new-app ${APP_NAME}
+oc new-app ${APP_NAME}
 
-# oc expose service ${APP_NAME}
-# oc create route edge coolstore-quarkus \
-#   --service=coolstore-quarkus \
-#   --port=8080-tcp
+oc expose service ${APP_NAME}
+oc create route edge coolstore-quarkus \
+  --service=coolstore-quarkus \
+  --port=8080-tcp
